e3269db2343640339cbec166e67ab6fdfd041ca5
bing050802 <bing050802@163.com>
2018-05-16 11:57:58 +0800

    创建一键脚本
    参数为 配置目录和工程目录，直接产生ipa包

---
+++ soures/easy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#  easy.sh
+#  PrivilegedTaskExample
+#
+#  Created by bing050802 on 2018/5/16.
+#  Copyright © 2018年 Sveinbjorn Thordarson. All rights reserved.
+SETTINGPATH=$1
+PROJECTPATH=$2
+contentPath=./content.txt
+contentjsonPath=./Contents.json
+exporpplist=./exportTest
+main(){
+    ./read.sh $contentPath  $SETTINGPATH/cloud_ico.png
+    ./copySource.sh $SETTINGPATH $PROJECTPATH $contentjsonPath
+   ./productIPA.sh $SETTINGPATH $PROJECTPATH $exporpplist
+
+}
+main()
