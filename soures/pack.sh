54fac01c92a5740d58476682a54b492ffc191aea
bing050802 <bing050802@163.com>
2018-06-15 11:40:28 +0800

    打包取版本号

--- soures/pack.sh
+++ soures/pack.sh
@@ -7,10 +7,15 @@
 #  Copyright © 2018年 Sveinbjorn Thordarson. All rights reserved.
 SETTINGPATH=$1
 PROJECTPATH=$2
+Version=$3
 exporpplist=./exportTest.plist
 main(){
     cp -f $exporpplist $SETTINGPATH
-    ./productIPA.sh $SETTINGPATH $PROJECTPATH $SETTINGPATH/exportTest.plist
+    cp -f $PROJECTPATH/Cloudoc2/YHZConfig.plist $SETTINGPATH
+    local version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${PROJECTPATH}/Cloudoc2/Info.plist")
+    /usr/libexec/PlistBuddy -c "Set :YHZConfig_Version ${version}" "${SETTINGPATH}/YHZConfig.plist"
+
+    ./productIPA.sh $SETTINGPATH $PROJECTPATH "${SETTINGPATH}/exportTest.plist"
 
 }
 main
